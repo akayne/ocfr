12a139914d2cdfc770a247ba5eef6cb453d59222
alyssa-kayne <akayne@iu.edu>
2019-10-02 13:46:55 -0400

    Beginning of stuff

---
+++ dummy.php
@@ -0,0 +1,40 @@
+[
+  {
+    "personId": "SOME-REALLY-LONG-1234",
+    "firstName": "Kathryn",
+    "lastName": "Pryde",
+    "radioNumber": "A-1",
+    "stationNumber": "all",
+    "isActive": TRUE,
+    "address": "1123 Xavier School Drive, Watkinsville, GA 30677",
+    "email": "kpryde@ocfr.com",
+    "phone": "7075551234",
+    "dob": "1967-12-23",
+    "startDate": "1997-1-13",
+    "gender": "Female",
+    "position": "Chief"
+  },
+  {
+    "patientGuid": "SOME-REALLY-SHORT-5678",
+    "firstName": "Piotr",
+    "lastName": "Rasputin",
+    "radioNumber": "A-1",
+    "stationNumber": "all",
+    "isActive": TRUE,
+    "address": "1123 Xavier School Drive, Watkinsville, GA 30677",
+    "email": "kpryde@ocfr.com",
+    "phone": "7075551234",
+    "dob": "1967-12-23",
+    "startDate": "1997-1-13",
+    "gender": "Female",
+    "position": "Chief"
+  },
+  {
+    "patientGuid": "SOME-UNIQUE-ABCDE1",
+    "firstName": "John",
+    "lastName": "Doe",
+    "dob": "1997",
+    "sexAtBirth": "M",
+    "priority": "low"
+  }
+]
