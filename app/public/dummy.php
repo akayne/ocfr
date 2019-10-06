464c684f8285890cd7034055b1542759935e8f41
Roxie Liu <roxiliu@iu.edu>
2019-10-06 16:21:06 -0400

    update dummy date

--- app/public/dummy.php
+++ app/public/dummy.php
@@ -1,6 +1,6 @@
 [
   {
-    "personId": "SOME-REALLY-LONG-1234",
+    "personId": "1",
     "firstName": "Kathryn",
     "lastName": "Pryde",
     "radioNumber": "A-1",
@@ -15,7 +15,7 @@
     "position": "Chief"
   },
   {
-    "personId": "SOME-REALLY-SHORT-5678",
+    "personId": "2",
     "firstName": "Piotr",
     "lastName": "Rasputin",
     "radioNumber": "841",
@@ -30,7 +30,7 @@
     "position": "Firefighter"
   },
   {
-    "personId": "SOME-UNIQUE-ABCDE1",
+    "personId": "3",
     "firstName": "Warren",
     "lastName": "Worthington III",
     "radioNumber": "122",
@@ -62,77 +62,77 @@
 
 [
   {
-    "certId": "cert-1-1-1",
+    "certId": "1",
     "name": "CPR for Healthcare Providers",
     "agency": "American Heart Association",
     "defaultExpiration": "2"
   },
 
   {
-    "certId": "cert-2-2-2",
+    "certId": "2",
     "name": "CPR for the Professional Rescuer",
     "agency": "American Red Cross",
     "defaultExpiration": "2"
   },
 
   {
-    "certId": "cert-3-3-3",
+    "certId": "3",
     "name": "Firefighter I",
     "agency": "Athens Technical College",
     "defaultExpiration": "3"
   },
 
   {
-    "certId": "cert-4-4-4",
+    "certId": "4",
     "name": "Firefighter I",
     "agency": "Ivy Technical College",
     "defaultExpiration": "3"
   },
 
   {
-    "certId": "cert-5-5-5",
+    "certId": "5",
     "name": "POST",
     "agency": "Georgia POST Academy",
     "defaultExpiration": "5"
   },
 
   {
-    "certId": "cert-6-6-6",
+    "certId": "6",
     "name": "Paramedic",
     "agency": "Ivy Technical College",
     "defaultExpiration": "3"
   },
 
   {
-    "certId": "cert-7-7-7",
+    "certId": "7",
     "name": "Due Regard",
     "agency": "Athens Technical College",
     "defaultExpiration": "2"
   },
 
   {
-    "certId": "cert-8-8-8",
+    "certId": "8",
     "name": "EMT-Adv",
     "agency": "Athens Technical College",
     "defaultExpiration": "1"
   },
 
   {
-    "certId": "cert-9-9-9",
+    "certId": "9",
     "name": "Firefighter II",
     "agency": "Indiana University Indianapolis",
     "defaultExpiration": "2"
   },
 
   {
-    "certId": "cert-10-10-10",
+    "certId": "10",
     "name": "HAZMAT",
     "agency": "Indiana University Indianapolis",
     "defaultExpiration": "10"
   },
 
   {
-    "certId": "cert-11-11-11",
+    "certId": "11",
     "name": "Extrication",
     "agency": "Athens Technical College",
     "defaultExpiration": ""
@@ -153,69 +153,69 @@
 
 [
   {
-    "personId": "SOME-REALLY-LONG-1234",
-    "certId": "cert-9-9-9",
+    "personId": "1",
+    "certId": "9",
     "dateReceived": "2018-08-01",
     "dateExpired": "2020-08-01"
   },
   {
-    "personId": "SOME-REALLY-LONG-1234",
-    "certId": "cert-2-2-2",
+    "personId": "1",
+    "certId": "2",
     "dateReceived": "2017-07-01",
     "dateExpired": "2019-07-01"
   },
   {
-    "personId": "SOME-REALLY-LONG-1234",
-    "certId": "cert-10-10-10",
+    "personId": "1",
+    "certId": "10",
     "dateReceived": "2010-02-01",
     "dateExpired": "2020-02-01"
   },
   {
-    "personId": "SOME-REALLY-LONG-1234",
-    "certId": "cert-11-11-11",
+    "personId": "1",
+    "certId": "11",
     "dateReceived": "2006-06-01",
     "dateExpired": ""
   },
 
   {
-    "personId": "SOME-REALLY-SHORT-5678",
-    "certId": "cert-8-8-8",
+    "personId": "2",
+    "certId": "8",
     "dateReceived": "2019-09-01",
     "dateExpired": "2020-09-01"
   },
   {
-    "personId": "SOME-REALLY-SHORT-5678",
-    "certId": "cert-1-1-1",
+    "personId": "2",
+    "certId": "1",
     "dateReceived": "2019-07-01",
     "dateExpired": "2021-07-01"
   },
   {
-    "personId": "SOME-REALLY-SHORT-5678",
-    "certId": "cert-7-7-7",
+    "personId": "2",
+    "certId": "7",
     "dateReceived": "2019-10-01",
     "dateExpired": "2021-10-01"
   },
   {
-    "personId": "SOME-UNIQUE-ABCDE1",
-    "certId": "cert-6-6-6",
+    "personId": "3",
+    "certId": "6",
     "dateReceived": "2017-09-01",
     "dateExpired": "2020-09-01"
   },
   {
-    "personId": "SOME-UNIQUE-ABCDE1",
+    "personId": "3",
     "certId": "cert-1-1-1",
     "dateReceived": "2018-07-01",
     "dateExpired": "2020-07-01"
   },
   {
-    "personId": "SOME-UNIQUE-ABCDE1",
-    "certId": "cert-7-7-7",
+    "personId": "3",
+    "certId": "7",
     "dateReceived": "2017-10-01",
     "dateExpired": "2019-10-01"
   },
   {
-    "personId": "SOME-UNIQUE-ABCDE1",
-    "certId": "cert-3-3-3",
+    "personId": "3",
+    "certId": "3",
     "dateReceived": "2017-08-01",
     "dateExpired": "2020-08-01"
   }
