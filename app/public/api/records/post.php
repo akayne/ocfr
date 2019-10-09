c2742c32cadf5eb26bd4d437b48f2d9d618b2a20
Roxie Liu <roxiliu@iu.edu>
2019-10-09 14:36:52 -0400

    certificaiton button 1

---
+++ app/public/api/records/post.php
@@ -0,0 +1,22 @@
+<?php
+
+// 0. Validate my data
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'INSERT INTO certifications
+    (name, agency, defaultExpiration)
+  VALUES (?,?,?)'
+);
+$stmt->execute([
+  $_POST['name'],
+  $_POST['agency'],
+  $_POST['defaultExpiration']
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/');
