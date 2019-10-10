d7db71f8c72606a6a3321a1faa7edd83b20d3283
Roxie Liu <roxiliu@iu.edu>
2019-10-10 15:41:10 -0400

    commit for not add the right thing, it add the first record everytime

--- app/public/js/certification.app.js
+++ app/public/js/certification.app.js
@@ -10,7 +10,8 @@ var certificationApp = new Vue({
       fetch('api/certification/')
       .then(response => response.json())
       .then(json => { certificationApp.certifications = json })
-  },
+    },
+
     handleCreate(event) {
       fetch('api/certification/post.php', {
         method:'POST',
@@ -26,10 +27,19 @@ var certificationApp = new Vue({
         console.error(err);
       });
 
-},
-
+      this.handleReset();
+    },
+    handleReset() {
+      this.recordCertif = {
+        name: '',
+        agency: '',
+        defaultExpiration: ''
+      }
+    }
+  },//end of methods
 
   created() {
     this.fetchcertifications();
   }
+
 });
