0f29a53b482e1d58ced69f6992b5d8b92837238c
alyssa-kayne <akayne@iu.edu>
2019-10-13 14:35:57 -0400

    Working on the employee edit button

--- app/public/api/employee/post.php
+++ app/public/api/employee/post.php
@@ -8,9 +8,9 @@ $db = DbConnection::getConnection();
 
 // Step 2: Prepare & run the query
 $stmt = $db->prepare(
-  'INSERT INTO employees
+  'UPDATE employees
     (personId, firstName, lastName, radioNumber, stationNumber,isActive, address,email,phone,dob,startDate,gender,position)
-  VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)'
+  SET (?,?,?,?,?,?,?,?,?,?,?,?,?)'
 );
 
 $guid = Uuid::uuid4()->toString();
@@ -20,15 +20,7 @@ $stmt->execute([
   $_POST['firstName'],
   $_POST['lastName'],
   $_POST['radioNumber'],
-  $_POST['stationNumber'],
-  $_POST['isActive'],
-  $_POST['address'],
-  $_POST['email'],
-  $_POST['phone'],
-  $_POST['dob'],
-  $_POST['startDate'],
-  $_POST['gender'],
-  $_POST['position']
+  $_POST['stationNumber']
 
 
 ]);
